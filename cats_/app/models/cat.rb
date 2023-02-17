502c65cc15dd8326ea58d3deb5491a0727f40d68
andy <morrison.andrew422@gmail.com>
2023-02-17 17:49:03 -0500

    validation

--- cats_/app/models/cat.rb
+++ cats_/app/models/cat.rb
@@ -4,9 +4,15 @@ class Cat < ApplicationRecord
   validates :birth_date, presence: true
   validates :name, presence: true
   validates :sex, presence: true, includsion: { in: ["M", "F"] }
+  validate :birth_date_cannot_be_future
 
-  def :birth_date_cannot_be_future
-
-    # if :birth_date  = "#{t.year}-#{t.month}-#{t.day}"
+  def birth_date_cannot_be_future
+    if birth_date.present? && birth_date > Date.today
+      errors.add(:birth_date, "cant be in the future")
+    end
   end
+
+  def time_ago_in_words
+  end
+
 end
